ed8c5a03bc1bcad3c001b446808fb4c23c9c3e36
thanithamizhakarathikalanjiyam <pitchaimbox-github@yahoo.com>
2019-06-08 13:05:40 +0530

    a* series

--- git_agarathi_eng_upload.bat
+++ git_agarathi_eng_upload.bat
@@ -1,8 +1,7 @@
-cd search
-d:
 for %%a in (a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z) do (  
 set foo=%%a
-git add %%a*.md 
+cd /D/GitRepo/agarathi/search/%%a
+git add %%a*
 git commit -am "%%a* series" 
 git push 
 )
